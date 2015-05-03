a16bd2dc7115b4d5967aa03a3193f4d526f90e7f
Elena Jung <mail@elenajung.de>
2015-05-03 19:08:16 +0200

    adding comments to each mathematical line

--- ex3/ex3.rb
+++ ex3/ex3.rb
@@ -1,23 +1,28 @@
 puts "I will now count my chickens:"
 
+# prints the number of hens and roosters
 puts "Hens #{25 + 30 / 6}"
 puts "Roosters #{100 - 25 * 3 % 4}"
 
 puts "Now I will count the eggs:"
 
+# prints the number of eggs
 puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
 
+# compares 5 with -2
 puts "Is it true that 3 + 2 < 5 - 7?"
 
+# does the comparison and returns boolean
 puts 3 + 2 < 5 - 7
 
+# prints a mathematical question and returns the calculated answer with string interpolation
 puts "What is 3 + 2? #{3 + 2}"
 puts "What is 5 - 7? #{5 - 7}"
 
 puts "Oh, that's why it's false."
-
 puts "How about some more."
 
+# prints the question of comparison and returns boolean with string interpolation
 puts "Is it greater? #{5 > -2}"
 puts "Is it greater or equal? #{5 >= -2}"
 puts "Is it less or equal? #{5 <= -2}"
