6582afa6f432b53f1acfb36fa21cc4c21958dfeb
Elena Jung <mail@elenajung.de>
2015-05-03 20:03:56 +0200

    adding comments to all assignments

--- ex4/ex4.rb
+++ ex4/ex4.rb
@@ -1,10 +1,25 @@
+#amount of cars
 cars = 100
-space_in_a_car = 4
+
+#amount of places in one car
+space_in_a_car = 4.0
+
+#amount of drivers
 drivers = 30
+
+#amount of passengers
 passengers = 90
+
+#the numbers of all cars minus the working drivers returns the number of not driven cars
 cars_not_driven = cars - drivers
+
+#the cars in action are defined by the number of drivers
 cars_driven = drivers
+
+#the total number of passengers getting driven, is defined by the space in the cars in use
 carpool_capacity = cars_driven * space_in_a_car
+
+#the average amount of passengrs per car is defined by the total passengers and the cars in use
 average_passengers_per_car = passengers / cars_driven
 
 
