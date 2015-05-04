98c656d0dc4bc0f8223bfc4deaa5f579ebf02b40
Elena Jung <mail@elenajung.de>
2015-05-04 21:59:57 +0200

    adding comments

--- ex7/ex7.rb
+++ ex7/ex7.rb
@@ -1,8 +1,16 @@
+#printing a sting
 puts "Mary had a little lamb."
+
+#printing a string with string interpolation of another string 
 puts "Its fleece was white as #{'snow'}."
+
+#printing a string
 puts "And everywhere that Mary went."
+
+#printing a string a specific amount of times
 puts "." * 10  # what'd that do?
 
+#defining numbered variables to strings, in this case letters
 end1 = "C"
 end2 = "h"
 end3 = "e"
@@ -17,5 +25,8 @@ end11 = "e"
 end12 = "r"
 
 # watch that print vs. puts on this line what's it do?
+#printing the letters in one line
 print end1 + end2 + end3 + end4 + end5 + end6
+
+#printing one letter each line
 puts end7 + end8 + end9 + end10 + end11 + end12
