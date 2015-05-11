70cbb623d3022178f49503120a31d113031399d0
Elena Jung <mail@elenajung.de>
2015-05-11 19:50:37 +0200

    script with fewer arguments

---
+++ ex13/less.rb
@@ -0,0 +1,19 @@
+#ARGV = argument variable
+
+favorite_color_one, favorite_color_two  = ARGV
+
+puts """
+Strophe 1
+#{favorite_color_one}, #{favorite_color_one}, #{favorite_color_one} sind alle meine Kleider;
+#{favorite_color_one}, #{favorite_color_one}, #{favorite_color_one} ist alles was ich hab.
+Darum lieb ich alles, was so #{favorite_color_one} ist,
+weil mein Schatz ein #{favorite_color_one}ling ist.
+"""
+puts """
+Strophe 2
+#{favorite_color_two}, #{favorite_color_two}, #{favorite_color_two} sind alle meine Kleider;
+#{favorite_color_two}, #{favorite_color_two}, #{favorite_color_two} ist alles was ich hab.
+Darum lieb ich alles, was so #{favorite_color_two} ist,
+weil mein Schatz ein #{favorite_color_two}ling ist.
+"""
+#Question: Warum funktioniert das nur mit mehreren Variablen? 
