e9f96e92173da24cba58f8c814bd34b6bdabcc77
Elena Jung <mail@elenajung.de>
2015-05-11 19:38:18 +0200

    more than one argument

--- ex13/ex13.rb
+++ ex13/ex13.rb
@@ -1,5 +1,9 @@
-first, second, third = ARGV
+#ARGV = argument variable
+
+first, second, third, blub  = ARGV
 
 puts "Your first variable is: #{first}"
 puts "Your second variable is: #{second}"
 puts "Your third variable is: #{third}"
+puts "Your third variable is: #{blub}"
+
