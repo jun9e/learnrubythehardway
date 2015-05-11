21078c9d9f4c99dbb70a7248bdaf63de2106a51a
Elena Jung <mail@elenajung.de>
2015-05-11 19:27:35 +0200

    using floats and giving back 10% change

--- ex12/ex12.rb
+++ ex12/ex12.rb
@@ -1,12 +1,19 @@
 print "Give me a number: "
-number = gets.chomp.to_i
+number = gets.chomp.to_f
 
 bigger = number * 100
 puts "A bigger number is #{bigger}."
 
 print "Give me another number: "
 another = gets.chomp
-number = another.to_i
+number = another.to_f
 
 smaller = number / 100
 puts "A smaller number is #{smaller}."
+
+#to_f changes the integer to a float
+
+print "Give me some money: "
+money = gets.chomp.to_f
+change = money * 0.1
+puts "Here is your change: $#{change}"
