00fdff20d610580ca3a660b8b253b466188acfa9
Elena Jung <mail@elenajung.de>
2015-05-11 20:03:13 +0200

    combning ARGV and gets.chomp

--- ex13/more.rb
+++ ex13/more.rb
@@ -10,4 +10,15 @@ puts"""
 #{item4}
 #{item5}
 """
+puts"Do you want to add something to your list?"
+additional_item = gets.chomp
 
+puts "Your Grocery Shopping List:"
+puts item1
+puts item2
+puts item3
+puts item4
+puts item5
+puts additional_item
+
+#Question: Warum funktioniert das nicht?
