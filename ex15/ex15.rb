6d47ac50f3fa97591c7e20e300bfe5be9712b4c1
Elena Jung <mail@elenajung.de>
2015-05-11 20:41:14 +0200

    adding comments to each line

--- ex15/ex15.rb
+++ ex15/ex15.rb
@@ -1,13 +1,19 @@
+#gets the filename from the comand line and saves it in the variable filename
 filename = ARGV.first
 
+#opens the file and gets saved in txt
 txt = open(filename)
 
+#reads the content of txt (filename) and prints it
 puts "Here's your file #{filename}:"
 print txt.read
 
+#asks for the filename within the programm and saves it in file_again
 print "Type the filename again: "
 file_again = $stdin.gets.chomp
 
+#opens the file and gets saved in txt
 txt_again = open(file_again)
 
+#reads the content again and prints it
 print txt_again.read
