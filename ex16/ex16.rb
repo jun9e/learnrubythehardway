6b94f557ba25d70fbd264f5a69d2f2b84c4cb153
Elena Jung <mail@elenajung.de>
2015-05-12 20:09:07 +0200

    answering questions

--- ex16/ex16.rb
+++ ex16/ex16.rb
@@ -25,21 +25,28 @@ line3 = $stdin.gets.chomp
 
 puts "I'm going to write these to the file."
 
+=begin
 target.write(line1)
 target.write("\n")
 target.write(line2)
 target.write("\n")
 target.write(line3)
 target.write("\n")
+=end
+
 
 #Question: How to do task 3
 #There's too much repetition in this file. Use strings, formats, and escapes to print out line1, line2, and line3 with just one target.write() command instead of six.
+=begin
 formatter = "%{first} %{second} %{third}"
-puts formatter % {
+target.write(formatter % {
   first: "#{line1}\n",
-  second: "#{line2}}\n",
-  third: "#{line3}}\n",
-}
+  second: "#{line2}\n",
+  third: "#{line3}\n",
+})
+=end
+
+target.write("#{line1}\n#{line2}\n#{line3}\n")
 
 puts "And finally, we close it."
 target.close
