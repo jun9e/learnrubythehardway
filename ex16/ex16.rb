def0d6a9b563c0313ea33537624f99576f5dc6e6
Elena Jung <mail@elenajung.de>
2015-05-12 13:22:58 +0200

    adding file open modes

--- ex16/ex16.rb
+++ ex16/ex16.rb
@@ -8,6 +8,7 @@ puts "If you do want that, hit RETURN."
 $stdin.gets
 
 puts "Opening the file..."
+#w = write only
 target = open(filename, 'w')
 
 puts "Truncating the file.  Goodbye!"
@@ -42,3 +43,35 @@ puts formatter % {
 
 puts "And finally, we close it."
 target.close
+
+=begin
+
+Mode |  Meaning
+-----+--------------------------------------------------------
+"r"  |  Read-only, starts at beginning of file  (default mode).
+-----+--------------------------------------------------------
+"r+" |  Read-write, starts at beginning of file.
+-----+--------------------------------------------------------
+"w"  |  Write-only, truncates existing file
+     |  to zero length or creates a new file for writing.
+-----+--------------------------------------------------------
+"w+" |  Read-write, truncates existing file to zero length
+     |  or creates a new file for reading and writing.
+-----+--------------------------------------------------------
+"a"  |  Write-only, starts at end of file if file exists,
+     |  otherwise creates a new file for writing.
+-----+--------------------------------------------------------
+"a+" |  Read-write, starts at end of file if file exists,
+     |  otherwise creates a new file for reading and
+     |  writing.
+-----+--------------------------------------------------------
+ "b" |  Binary file mode (may appear with
+     |  any of the key letters listed above).
+     |  Suppresses EOL <-> CRLF conversion on Windows. And
+     |  sets external encoding to ASCII-8BIT unless explicitly
+     |  specified.
+-----+--------------------------------------------------------
+ "t" |  Text file mode (may appear with
+     |  any of the key letters listed above except "b").
+
+=end
